f9c1cfd7c9f1c912a1e818cd757cdd19e6fdf5de
ishidu-ryota-fz <ishidu-ryota-fz@ynu.jp>
2017-07-30 15:37:50 +0900

    twospiral dataset

--- FeedforwardNeuralNetwork/TwoSpiralDataSet.cpp
+++ FeedforwardNeuralNetwork/TwoSpiralDataSet.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include "TwoSpiralDataSet.h"
+#include <fstream>
 
+#define POINTS 96
+#define LAP 3
+#define MINDIAMETER 1.0
+#define MAXDIAMETER 6.0
 
 TwoSpiralDataSet::TwoSpiralDataSet()
 {
@@ -9,24 +14,23 @@ TwoSpiralDataSet::TwoSpiralDataSet()
 
 void TwoSpiralDataSet::load()
 {
-	int points = 96;
-	int density = 2;
-	double maxDiameter = 1.0;
-	int dataNum = points * density;
+	double radiusGrowth = (MAXDIAMETER - MINDIAMETER) / double(POINTS);
+	double angleGrowth = 2.0 * M_PI / double(POINTS) * double(LAP);
+	int dataNum = POINTS * 2;
 	dataSet.resize(dataNum, 2);
 	teachSet.resize(dataNum, 1);
-	for (int i = 0; i < density; ++i)
+	std::ofstream ofs("TwoSpiralData.csv");
+	ofs << "x, y, val" << std::endl;
+	for (int i = 0; i < POINTS; ++i)
 	{
-		for (int j = 0; j < points; ++j)
-		{
-			const double angle = j * M_PI / (16.0 * density);
-			const double radius = maxDiameter * (104 * density - i) / (104 * density);
-			const double x = radius * cos(angle);
-			const double y = radius * sin(angle);
-			dataSet.row(i * points + j) << x , y;
-
-			teachSet.row(i * points + j) << i;
-		}
+		const double x = (radiusGrowth * i + MINDIAMETER) * cos(angleGrowth * i);
+		const double y = (radiusGrowth * i + MINDIAMETER) * sin(angleGrowth * i);
+		ofs << x << ", " << y << ", " << 1 << std::endl;
+		ofs << -x << ", " << -y << ", " << 0 << std::endl;
+		dataSet.row(2 * i) << x , y;
+		dataSet.row(2 * i + 1) << -x , -y;
+		teachSet.row(2 * i) << 1;
+		teachSet.row(2 * i + 1) << 0;
 	}
 	testDataSet = dataSet;
 	testTeachSet = teachSet;
