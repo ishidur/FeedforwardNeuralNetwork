d47e26ff1d18c5232d5392fa9658abd435d51760
Ryota Ishidu <ishidu-ryota-fz@ynu.jp>
2018-09-25 14:51:15 +0900

    やべえ

--- FeedforwardNeuralNetwork/config.h
+++ FeedforwardNeuralNetwork/config.h
@@ -14,17 +14,17 @@ bool needPretrain = false;
 #define PRETRAIN_LEARNING_TIME 1000
 //#define PRETRAIN_LEARNING_TIME 100000000
 
-////XOR
-//const std::vector<double> initVals = {0.01};
-//#define TRIALS_PER_STRUCTURE 5
-//#define LEARNING_RATE 1.0
-//#define LEARNING_TIME 100000
-//#define ERROR_BOTTOM 0.01
-////dataset
-//XORDataSet dataSet;
-////Network structure.
-//const std::vector<std::vector<int>> structures = {{2, 3, 3, 1}};
-////const std::vector<std::vector<int>> structures = {{2, 2, 1},{2, 3, 1},{2, 4, 1},{ 2, 2, 2, 1 },{ 2, 3, 3, 1 },{ 2, 4, 4, 1 }, {2, 2, 2, 2, 1},{2, 3, 3, 3, 1},{2, 4, 4, 4, 1}};
+//XOR
+const std::vector<double> initVals = {0.001};
+#define TRIALS_PER_STRUCTURE 5
+#define LEARNING_RATE 0.5
+#define LEARNING_TIME 10000
+#define ERROR_BOTTOM -0.01
+//dataset
+XORDataSet dataSet;
+//Network structure.
+const std::vector<std::vector<int>> structures = {{2, 3, 1}};
+//const std::vector<std::vector<int>> structures = {{2, 2, 1},{2, 3, 1},{2, 4, 1},{ 2, 2, 2, 1 },{ 2, 3, 3, 1 },{ 2, 4, 4, 1 }, {2, 2, 2, 2, 1},{2, 3, 3, 3, 1},{2, 4, 4, 4, 1}};
 
 ////Function approximation
 //const std::vector<double> initVals = {0.01};
@@ -47,17 +47,17 @@ bool needPretrain = false;
 //MNISTDataSet dataSet;
 ////Network structure.
 //const std::vector<std::vector<int>> structures = {{784, 10}};
-
-//TwoSpiral Prpblem
-const std::vector<double> initVals = {1.0};
-#define TRIALS_PER_STRUCTURE 1
-#define LEARNING_RATE 0.05
-#define LEARNING_TIME 100000
-#define ERROR_BOTTOM 0.001
-TwoSpiralDataSet dataSet;
-const std::vector<std::vector<int>> structures = {
-	{2, 5, 5, 5, 1}, {2, 10, 10, 10, 1}, {2, 15, 15, 15, 1}, {2, 20, 20, 20, 1}
-};
+//
+// //TwoSpiral Prpblem
+// const std::vector<double> initVals = {1.0};
+// #define TRIALS_PER_STRUCTURE 1
+// #define LEARNING_RATE 0.05
+// #define LEARNING_TIME 100000
+// #define ERROR_BOTTOM 0.001
+// TwoSpiralDataSet dataSet;
+// const std::vector<std::vector<int>> structures = {
+// 	{2, 5, 5, 5, 1}, {2, 10, 10, 10, 1}, {2, 15, 15, 15, 1}, {2, 20, 20, 20, 1}
+// };
 //const std::vector<std::vector<int>> structures = {{2, 20, 20, 20, 1}};
 
 inline Eigen::VectorXd activationFunc(Eigen::VectorXd const& inputs)
